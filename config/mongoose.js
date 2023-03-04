84ebef3ef8ad3bae21dacc2bc0cd7f4dd42aa2af
Raunak_Kumari <raunak@sampana.in>
2023-03-04 10:18:45 +0530

    first commit

---
+++ config/mongoose.js
@@ -0,0 +1,19 @@
+
+const mongoose = require('mongoose');
+
+// connect from mongodb
+ //mongoose.connect('mongodb://localhost:27017/EmployeeReviewSystem');
+
+mongoose.connect('mongodb+srv://raunak:atlast@cluster0.sz6kdtn.mongodb.net/test')
+// aquire connection if it is succesful
+const db = mongoose.connection;
+
+// error
+db.on('error', console.error.bind(console, 'error connecting to db'));
+
+// up and running then print the message
+db.once('open', function(){
+    console.log("successfully connected to database!");
+});
+
+module.exports = db;
---
+++ routes/review.js
@@ -0,0 +1,14 @@
+const express = require("express");
+// incude passport 
+const passport = require("passport");
+// here using of express router 
+const router = express.Router();
+// include review conttroller 
+const reviewController = require('../controllers/review-controller');
+
+// for creating new rweview at this end point 
+router.get('/newReview/:id', reviewController.createReview);
+
+module.exports = router;
+
+
